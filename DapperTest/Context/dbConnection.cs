b7d57273c6a3813af841e0844724b78b1efafd93
ueverson diego soares pedroso <udanjo_hyuuga@hotmail.com>
2020-10-08 14:46:43 -0300

    Ajuste

--- DapperTest/Context/dbConnection.cs
+++ DapperTest/Context/dbConnection.cs
@@ -7,6 +7,6 @@ namespace DapperTest.Context
     public class dbConnection : IConnectionFactory
     {
         public IDbConnection Connection() 
-            => new SqlConnection(@"Server=tcp:myfinanceju.database.windows.net,1433;Initial Catalog=MyFinance;Persist Security Info=False;User ID=udanjo;Password=udsp@2019;MultipleActiveResultSets=False;Encrypt=True;TrustServerCertificate=False;Connection Timeout=30;");
+            => new SqlConnection(@"Server=teste.database.windows.net,1433;Initial Catalog=MyFinance;Persist Security Info=False;User ID=******;Password=******;MultipleActiveResultSets=False;Encrypt=True;TrustServerCertificate=False;Connection Timeout=30;");
     }
 }
